5c8c7b0bdadb86aa37d4a9fb09b936d65fb9c53c
Literall <Petey@Peters-MacBook-Air.local>
2017-10-13 00:30:07 -0700

    more changes to application

---
+++ src/components/UserListElement.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class UserListElement extends React.Component {
+    render() {
+
+        const user = this.props.user
+
+        return (
+            <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.username}</td>
+                <td>{user.job}</td>
+                <td><a href={'/users-edit/' + user.id}>Edit</a></td>
+                <td><button data-id={user.id}>Delete</button></td>
+            </tr>
+        )
+    }
+}
+
+UserListElement.propTypes = {
+    user: React.PropTypes.object.isRequired
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,43 +1,30 @@
 import React from 'react';
+import UserList from './UserList';
 
 export default class App extends React.Component {
-    render() {
+    constructor(props) {
+        super(props);
+
         const users = [];
 
-        for(var i = 0; i < 10; i++) {
+        for (var i = 0; i < 10; i++) {
             users.push({
                 id: i + 1,
-                username: 'John' + i + 1,
-                job: 'Employee' + i + 1
+                username: 'John' + i,
+                job: 'Employee' + i
             });
         }
+
+        this.state = {
+            users: users,
+        }
+    }
+    render() {
+
         return (
-          <div>
-              <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Username</th>
-                            <th>Job</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {users.map((user, index) => {
-                        return (
-                            <tr key={user.id}>
-                                <td>{user.id}</td>
-                                <td>{user.username}</td>
-                                <td>{user.job}</td>
-                                <td><a href={'/users-edit/' + user.id }>Edit</a></td>
-                                <td><button data-id={user.id}>Delete</button></td>
-                            </tr>
-                        );
-                    })}
-                    </tbody>
-                </table>
-          </div>
+            <div>
+               <UserList users={this.state.users}/>
+            </div>
         )
     }
 }
